6a37fa9dd6b1e8bbc0130e3bf4efde6e27316051
Freek van de Ven <freekvandeven14@gmail.com>
2026-05-15 16:08:25 +0200

    feat: initial flutter create

---
+++ android/app/src/main/kotlin/nl/freekvandeven/notification_workflow/MainActivity.kt
@@ -0,0 +1,5 @@
+package nl.freekvandeven.notification_workflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // TODO(freek): Replace blank test with actual tests
+  test('Blank Test', () {
+    expect(true, true);
+  });
+}
